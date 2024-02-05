da422a57bcd1b8824a8512be935660b66e4df9cb
Oksana Kormilina <keysb@mail.ru>
2024-02-05 14:12:22 +0400

    Add CartContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
+import { useContext } from 'react'
+
 import Button from '../UI/Button'
 import logoImg from'../assets/logo.jpg'
+import CartContext from '../store/CartContext'
 
 export default function Header(){
+const cartCtx = useContext(CartContext)
+const totalCartItems = cartCtx.items.reduce((acc, item) => acc + item.quantity, 0)
+
     return <header id="main-header" >
         <div id="title">
             <img src={logoImg} alt="A Restaurant"/>
             <h1>ReactFood</h1>
         </div>
         <nav>
-            <Button textOnly>Cart (0)</Button>
+            <Button textOnly>Cart ({totalCartItems})</Button>
         </nav>
     </header>
 }
