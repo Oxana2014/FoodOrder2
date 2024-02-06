1740ffb719ea8559cd8642ad7e9c2cae6d67fe51
Oksana Kormilina <keysb@mail.ru>
2024-02-06 13:18:05 +0400

    Add CartItem, Checkout and Input

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,37 +1,45 @@
 import { useContext } from "react";
 
 import Modal from "../UI/Modal.jsx";
+import { currencyFormatter } from "../util/formatting.js";
+import Button from "../UI/Button.jsx";
+import CartItem from "./CartItem.jsx";
 import CartContext from "../store/CartContext.jsx";
-import {currencyFormatter} from '../util/formatting.js'
-import Button from "../UI/Button.jsx"
 import UserProgressContext from "../store/UserProgressContext.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
-  const userProgressCtx = useContext(UserProgressContext)
+  const userProgressCtx = useContext(UserProgressContext);
 
   const cartTotal = cartCtx.items.reduce(
-    (acc, item) => acc + item.quantity + item.price,
+    (acc, item) => acc + item.quantity * item.price,
     0
   );
   function handleCloseCart() {
-    userProgressCtx.hideCart()
+    userProgressCtx.hideCart();
   }
 
   return (
-    <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            {...item}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly onClick={handleCloseCart}>Close</Button>
-        <Button  onClick={handleCloseCart}>Go to Checkout</Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        {cartCtx.items.length > 0 && (
+          <Button onClick={handleCloseCart}>Go to Checkout</Button>
+        )}
       </p>
     </Modal>
   );
