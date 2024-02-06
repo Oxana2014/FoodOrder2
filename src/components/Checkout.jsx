ce0d5295920816b76d1df1a83672e75409657bc7
Oksana Kormilina <keysb@mail.ru>
2024-02-06 14:21:02 +0400

    Add some user progress handling

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -3,16 +3,24 @@ import Modal from "../UI/Modal";
 import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../util/formatting";
 import Input from "../UI/Input";
+import Button from "../UI/Button"
+import UserProgressContext from "../store/UserProgressContext";
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
+const userProgressCtx = useContext(UserProgressContext)
+
   const cartTotal = cartCtx.items.reduce(
     (acc, item) => acc + item.quantity * item.price,
     0
   );
 
+  function handleClose() {
+    userProgressCtx.hideCheckout()
+  }
+
   return (
-    <Modal>
+    <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
       <form>
         <h2>Checkout</h2>
         <p>Total Amount {currencyFormatter.format(cartTotal)}</p>
@@ -23,6 +31,10 @@ export default function Checkout() {
           <Input label="Postal Code" type="text" id="postal-code"/>
           <Input label="City" type="text" id="city"/>
         </div>
+        <p className="modal-actions">
+            <Button onClick={handleClose} type="button" textOnly>Close</Button>
+            <Button>Submit Order</Button>
+        </p>
       </form>
     </Modal>
   );
